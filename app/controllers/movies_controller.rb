77ad8b5a42d304a4b96c1208df8e501264fb7fbc
Karan Shetty <email": null>
2017-03-13 17:27:19 -0400

    Commented out render show since rails looks for it by default.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,8 @@ class MoviesController < ApplicationController
   def show
     #user response will be passed into the helper method created to get data from the API. A helper method will pull the necessary data regarding comments on the particular movie or show in question. The return result
     # @response =
-    render 'show'
+
+    #commented out the render show. Rails knows to look for the show page in the movies views. 
+    # render 'show'
   end
 end
