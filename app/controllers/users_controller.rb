16a0f545622b90acf5ac5a79449a89327396b5ca
Tatiana Abramova <email": null>
2017-03-13 15:53:40 -0400

    sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,39 @@
 class UsersController < ApplicationController
-  
-  def new
 
+  def new
+    @new_user = User.new
   end
 
   def create
+    @new_user = User.new(user_params)
+    if @new_user.save
+      session[:user_id] = @new_user.id
+      redirect_to user_path(@new_user.id)
+    else
+      render 'new'
+    end
   end
 
-  def edit
+  # def edit
 
-  end
+  # end
 
   def show
 
   end
 
-  def update
+  # def update
 
-  end
+  # end
 
   def destroy
 
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||=User.find_by_id(session[:user_id])
+  end
+
+  def require_user
+    redirect_to root_path unless current_user
+  end
+
+  def authorized
+    current_user.id == @wish_list.user_id
+  end
 end
