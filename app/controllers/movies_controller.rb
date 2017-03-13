c3908e61ed238bf160bb3be06b251835930162cb
Karan Shetty <email": null>
2017-03-13 17:25:01 -0400

    Create view for movie show. Have place holders for information.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,10 @@ class MoviesController < ApplicationController
     render 'index'
   end
 
-  def show
 
+  def show
+    #user response will be passed into the helper method created to get data from the API. A helper method will pull the necessary data regarding comments on the particular movie or show in question. The return result
+    # @response =
+    render 'show'
   end
 end
