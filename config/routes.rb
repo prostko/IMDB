65675cb56083b3328280c64908467a564051150b
Adedotun Oni <email": null>
2017-03-13 14:56:23 -0400

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :users
   resources :comments
-  resources :movies, only: [:show]
+  resources :movies, only: [:show, :index]
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
