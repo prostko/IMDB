d80641cbe9354494eecd7f294dd281926db6d655
Adedotun Oni <email": null>
2017-03-13 15:45:27 -0400

    Create Sessions login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(params [:email])
+    if @user && @user.authenticate(params[:password])
+      log_in user
+      redirect_to user
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to '/'
+  end
+
+
 end
