28220a360fc834b7910c5ae2a8f8260f7aedafb7
Adedotun <onidotun99@gmail.com>
2017-03-14 19:40:25 -0400

    User first test passes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let (:test_user) {User.new(name: "Carlos", email: "Carlos@gmail.com", password: "password" )}
   describe 'validations' do
-    context 'will raise an error if ' do
-      it 'when the name is empty'do
-
+      it 'saves a new user if successfully created'do
+        expect{test_user.save}.to change{User.count}.by(1)
       end
-    end
   end
 end
