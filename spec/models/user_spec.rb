9bf859911a1ca1562a29280e33ee9e629e1913f1
Adedotun <onidotun99@gmail.com>
2017-03-14 19:49:53 -0400

    Create second test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe User, type: :model do
       it 'saves a new user if successfully created'do
         expect{test_user.save}.to change{User.count}.by(1)
       end
+
+      it 'does not add a user to the database if they are not created'do
+        if  !test_user.save
+        expect{test_user.save}.to change{User.count}.by(0)
+      end
+    end
   end
 end
