eafff3b253474e4836e587877bd471713ae29252
Eric Prostko <ericprostko9@gmail.com>
2017-03-14 11:14:58 -0400

    Search now pulls from our database instead of the api

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,22 +1,21 @@
 class MoviesController < ApplicationController
   include ApplicationHelper
+
   def index
     render 'index'
   end
 
 
   def show
-    #user response will be passed into the helper method created to get data from the API. A helper method will pull the necessary data regarding comments on the particular movie or show in question. The return result
-    # @response =
-
-    #commented out the render show. Rails knows to look for the show page in the movies views.
-    # render 'show'
     @movie = Movie.find_by(id: params[:id])
   end
 
   def search
-
     @movies = get_movies_by_title(params['search_form'])
+
+    @movies.map! do |movie|
+      movie = Movie.find_by(imdbID: movie.imdbID)
+    end
 
     if @movies
       render "movies/index"
@@ -24,6 +23,10 @@ class MoviesController < ApplicationController
       @errors = 'Movie not found' # need to implement error message partials
       render 'movies/index'
     end
+  end
+
+  def like
+
   end
 
 end
