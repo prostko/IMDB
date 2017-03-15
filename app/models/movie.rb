f4f30299224805ac8cec052ccdf55d27c6b5ad13
Eric Prostko <ericprostko9@gmail.com>
2017-03-15 17:54:07 -0400

    asdf

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,7 +15,7 @@ class Movie < ApplicationRecord
     if first_word == 'The' || first_word == 'the'
       first_word = self.Title.split(' ')[1]
     end
-    related_pictures = Movie.where("movies.Title LIKE ?", "%" + first_word + "%").limit(7)
+    related_pictures = Movie.where("'movies.Title' LIKE ?", "%" + first_word + "%").limit(7)
   end
 
 private
